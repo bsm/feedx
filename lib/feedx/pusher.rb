b47e406e9eee958b9a2b736859d31cb891f84c9c
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2018-11-26 17:50:18 +0000

    Conditional push through metadata

--- lib/feedx/pusher.rb
+++ lib/feedx/pusher.rb
@@ -6,6 +6,8 @@ module Feedx
   class Pusher
     autoload :Recurring, 'feedx/pusher/recurring'
 
+    META_LAST_MODIFIED = 'X-Feedx-Pusher-Last-Modified'.freeze
+
     # See constructor.
     def self.perform(url, opts={}, &block)
       new(url, opts, &block).perform
@@ -16,6 +18,7 @@ module Feedx
     # @option opts [Enumerable,ActiveRecord::Relation] :enum relation or enumerator to stream.
     # @option opts [Symbol,Class<Feedx::Format::Abstract>] :format custom formatter. Default: from file extension.
     # @option opts [Symbol,Class<Feedx::Compression::Abstract>] :compress enable compression. Default: from file extension.
+    # @option opts [Time,Proc] :last_modified the last modified time, used to determine if a push is necessary.
     # @yield A block factory to generate the relation or enumerator.
     # @yieldreturn [Enumerable,ActiveRecord::Relation] the relation or enumerator to stream.
     def initialize(url, opts={}, &block)
@@ -25,18 +28,24 @@ module Feedx
       @blob     = BFS::Blob.new(url)
       @format   = detect_format(opts[:format])
       @compress = detect_compress(opts[:compress])
+      @last_mod = opts[:last_modified]
     end
 
-    def perform(enum=build_enum)
-      @blob.create do |io|
+    def perform
+      enum = @enum.is_a?(Proc) ? @enum.call : @enum
+      last_mod = @last_mod.is_a?(Proc) ? @last_mod.call(enum) : @last_mod
+      current  = (last_mod.to_f * 1000).floor
+
+      begin
+        previous = @blob.info.metadata[META_LAST_MODIFIED].to_i
+        return -1 unless current > previous
+      rescue BFS::FileNotFound # rubocop:disable Lint/HandleExceptions
+      end if current.positive?
+
+      @blob.create metadata: { META_LAST_MODIFIED => current.to_s } do |io|
         @compress.wrap(io) {|w| write_all(enum, w) }
       end
       @blob.info.size
-    end
-
-    # @return [Enumerable,ActiveRecord::Relation] the relation or enumerator.
-    def build_enum
-      @enum.is_a?(Proc) ? @enum.call : @enum
     end
 
     private
