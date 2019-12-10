cc10458bd2e8507848c235e14f1f57e459e89a87
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2019-12-10 15:24:49 +0000

    Fix timestamp comparisons

--- producer_test.go
+++ producer_test.go
@@ -2,6 +2,7 @@ package feedx_test
 
 import (
 	"context"
+	"sync/atomic"
 	"time"
 
 	"github.com/bsm/bfs"
@@ -13,11 +14,14 @@ import (
 var _ = Describe("Producer", func() {
 	var subject *feedx.Producer
 	var obj *bfs.Object
+	var numRuns uint32
 	var ctx = context.Background()
 
 	setup := func(o *feedx.ProducerOptions) {
 		var err error
 		subject, err = feedx.NewProducerForRemote(ctx, obj, o, func(w *feedx.Writer) error {
+			atomic.AddUint32(&numRuns, 1)
+
 			for i := 0; i < 10; i++ {
 				fix := fixture
 				if err := w.Encode(&fix); err != nil {
@@ -30,6 +34,7 @@ var _ = Describe("Producer", func() {
 	}
 
 	BeforeEach(func() {
+		atomic.StoreUint32(&numRuns, 0)
 		obj = bfs.NewInMemObject("path/to/file.jsonz")
 	})
 
@@ -54,16 +59,22 @@ var _ = Describe("Producer", func() {
 
 	It("should produce with custom last-mod check", func() {
 		setup(&feedx.ProducerOptions{
-			LastModCheck: func(_ context.Context) (time.Time, error) { return time.Unix(1515151515, 0), nil },
+			Interval:     50 * time.Millisecond,
+			LastModCheck: func(_ context.Context) (time.Time, error) { return time.Unix(1515151515, 987654321), nil },
 		})
 
-		Expect(subject.LastPush()).To(BeTemporally("~", time.Now(), time.Second))
-		Expect(subject.LastModified()).To(Equal(time.Unix(1515151515, 0)))
+		firstPush := subject.LastPush()
+		Expect(firstPush).To(BeTemporally("~", time.Now(), time.Second))
+		Expect(subject.LastModified()).To(Equal(time.Unix(1515151515, 987000000)))
 		Expect(subject.NumWritten()).To(Equal(10))
+		Expect(atomic.LoadUint32(&numRuns)).To(Equal(uint32(1)))
 
 		info, err := obj.Head(ctx)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(info.Size).To(BeNumerically("~", 75, 10))
-		Expect(info.Metadata).To(HaveKeyWithValue("X-Feedx-Last-Modified", "1515151515000"))
+		Expect(info.Metadata).To(HaveKeyWithValue("X-Feedx-Last-Modified", "1515151515987"))
+
+		Eventually(func() bool { return subject.LastPush().After(firstPush) }).Should(BeTrue())
+		Expect(atomic.LoadUint32(&numRuns)).To(Equal(uint32(1)))
 	})
 })
