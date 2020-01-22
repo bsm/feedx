49cbce4051095b0f4951cfa8ee41b45d2821fceb
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2020-01-22 10:23:36 +0000

    Force binary encoding on gzip

--- spec/feedx/compression/gzip_spec.rb
+++ spec/feedx/compression/gzip_spec.rb
@@ -5,11 +5,13 @@ RSpec.describe Feedx::Compression::Gzip do
     wio = StringIO.new
     described_class.writer(wio) {|w| w.write 'xyz' * 1000 }
     expect(wio.size).to be_within(20).of(40)
+    expect(wio.string.encoding).to eq(Encoding::BINARY)
 
     data = ''
     StringIO.open(wio.string) do |rio|
       described_class.reader(rio) {|z| data = z.read }
     end
     expect(data.size).to eq(3000)
+    expect(data.encoding).to eq(Encoding.default_external)
   end
 end
