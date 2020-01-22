c193c1dbc9260f794e8fc6682ea016093b977955
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2020-01-22 10:23:36 +0000

    Force binary encoding on gzip

--- lib/feedx/compression/gzip.rb
+++ lib/feedx/compression/gzip.rb
@@ -1,11 +1,25 @@
 require 'zlib'
 
 class Feedx::Compression::Gzip < Feedx::Compression::Abstract
-  def self.reader(io, &block)
-    Zlib::GzipReader.wrap(io, &block)
-  end
+  class << self
+    def reader(io, &block)
+      force_binmode(io)
+      Zlib::GzipReader.wrap(io, &block)
+    end
 
-  def self.writer(io, &block)
-    Zlib::GzipWriter.wrap(io, &block)
+    def writer(io, &block)
+      force_binmode(io)
+      Zlib::GzipWriter.wrap(io, &block)
+    end
+
+    private
+
+    def force_binmode(io)
+      if io.respond_to?(:binmode)
+        io.binmode
+      elsif io.respond_to?(:set_encoding)
+        io.set_encoding(Encoding::BINARY)
+      end
+    end
   end
 end
