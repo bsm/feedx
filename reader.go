d77ac338db9e15942ba41ae818e91075a47e5ec0
Andy Born <andy.born@blacksquaremedia.com>
2023-08-21 16:40:19 +0100

    add multi reader

--- reader.go
+++ reader.go
@@ -30,6 +30,121 @@ func (o *ReaderOptions) norm(name string) {
 
 // Reader reads data from a remote feed.
 type Reader struct {
+	ctx context.Context
+	opt *ReaderOptions
+
+	remotes  []*bfs.Object
+	cur      *streamReader
+	pos, num int
+}
+
+// NewReader inits a new reader.
+func NewReader(ctx context.Context, remote *bfs.Object, opt *ReaderOptions) (*Reader, error) {
+	return &Reader{
+		remotes: []*bfs.Object{remote},
+		opt:     opt,
+		ctx:     ctx,
+	}, nil
+}
+
+// MultiReader inits a new reader for multiple remotes.  Remotes are read sequentially as if concatenated.
+// Once all remotes are fully read, Read will return EOF.
+func MultiReader(ctx context.Context, remotes []*bfs.Object, opt *ReaderOptions) (*Reader, error) {
+	return &Reader{
+		remotes: remotes,
+		opt:     opt,
+		ctx:     ctx,
+	}, nil
+}
+
+// Read reads raw bytes from the feed.
+func (r *Reader) Read(p []byte) (int, error) {
+	if len(r.remotes) == 0 || r.pos >= len(r.remotes) {
+		return 0, io.EOF
+	}
+
+	r.ensureCurrent()
+	n, err := r.cur.Read(p)
+	if err == io.EOF {
+		r.cur = nil // remove current reader
+
+		// increment position and check if any more remotes
+		if r.pos++; r.pos < len(r.remotes) {
+			return r.Read(p) // start reading from next remote
+		}
+	}
+
+	return n, err
+}
+
+// Decode decodes the next formatted value from the feed.
+func (r *Reader) Decode(v interface{}) error {
+	if len(r.remotes) == 0 || r.pos >= len(r.remotes) {
+		return io.EOF
+	}
+
+	r.ensureCurrent()
+	err := r.cur.Decode(v)
+	if err == nil {
+		r.num++
+	} else if err == io.EOF {
+		r.cur = nil // remove current reader
+
+		// increment position and check if any more remotes
+		if r.pos++; r.pos < len(r.remotes) {
+			return r.Decode(v) // start decoding from next remote
+		}
+	}
+
+	return err
+}
+
+// NumRead returns the number of read values.
+func (r *Reader) NumRead() int {
+	return r.num
+}
+
+// LastModified returns the last modified time of the remote feed.
+func (r *Reader) LastModified() (time.Time, error) {
+	var lastMod timestamp
+	for _, remote := range r.remotes {
+		t, err := remoteLastModified(r.ctx, remote)
+		if err != nil {
+			return time.Time{}, err
+		}
+		if t > lastMod {
+			lastMod = t
+		}
+	}
+
+	return lastMod.Time(), nil
+}
+
+// Close closes the reader.
+func (r *Reader) Close() error {
+	if r.cur != nil {
+		return r.cur.Close()
+	}
+	return nil
+}
+
+func (r *Reader) ensureCurrent() {
+	if len(r.remotes) > 0 && r.cur == nil {
+		remote := r.remotes[r.pos]
+		var o ReaderOptions
+		if r.opt != nil {
+			o = *r.opt
+		}
+		o.norm(remote.Name())
+		r.cur = &streamReader{
+			remote: remote,
+			opt:    o,
+			ctx:    r.ctx,
+		}
+	}
+}
+
+type streamReader struct {
 	remote *bfs.Object
 	opt    ReaderOptions
 	ctx    context.Context
@@ -40,23 +155,8 @@ type Reader struct {
 	fd FormatDecoder
 }
 
-// NewReader inits a new reader.
-func NewReader(ctx context.Context, remote *bfs.Object, opt *ReaderOptions) (*Reader, error) {
-	var o ReaderOptions
-	if opt != nil {
-		o = *opt
-	}
-	o.norm(remote.Name())
-
-	return &Reader{
-		remote: remote,
-		opt:    o,
-		ctx:    ctx,
-	}, nil
-}
-
 // Read reads raw bytes from the feed.
-func (r *Reader) Read(p []byte) (int, error) {
+func (r *streamReader) Read(p []byte) (int, error) {
 	if err := r.ensureOpen(); err != nil {
 		return 0, err
 	}
@@ -65,7 +165,7 @@ func (r *Reader) Read(p []byte) (int, error) {
 }
 
 // Decode decodes the next formatted value from the feed.
-func (r *Reader) Decode(v interface{}) error {
+func (r *streamReader) Decode(v interface{}) error {
 	if err := r.ensureOpen(); err != nil {
 		return err
 	}
@@ -86,19 +186,8 @@ func (r *Reader) Decode(v interface{}) error {
 	return nil
 }
 
-// NumRead returns the number of read values.
-func (r *Reader) NumRead() int {
-	return r.num
-}
-
-// LastModified returns the last modified time of the remote feed.
-func (r *Reader) LastModified() (time.Time, error) {
-	lastMod, err := remoteLastModified(r.ctx, r.remote)
-	return lastMod.Time(), err
-}
-
 // Close closes the reader.
-func (r *Reader) Close() error {
+func (r *streamReader) Close() error {
 	var err error
 	if r.fd != nil {
 		if e := r.fd.Close(); e != nil {
@@ -118,7 +207,7 @@ func (r *Reader) Close() error {
 	return err
 }
 
-func (r *Reader) ensureOpen() error {
+func (r *streamReader) ensureOpen() error {
 	if r.br == nil {
 		br, err := r.remote.Open(r.ctx)
 		if err != nil {
--- reader_test.go
+++ reader_test.go
@@ -3,7 +3,6 @@ package feedx_test
 import (
 	"context"
 	"io"
-	"io/ioutil"
 	"time"
 
 	"github.com/bsm/bfs"
@@ -18,39 +17,80 @@ var _ = Describe("Reader", func() {
 	var obj *bfs.Object
 	var ctx = context.Background()
 
-	BeforeEach(func() {
-		obj = bfs.NewInMemObject("path/to/file.json")
-		Expect(writeMulti(obj, 3, time.Time{})).To(Succeed())
+	Describe("NewReader", func() {
+		BeforeEach(func() {
+			obj = bfs.NewInMemObject("path/to/file.json")
+			Expect(writeMulti(obj, 3, time.Time{})).To(Succeed())
 
-		var err error
-		subject, err = feedx.NewReader(ctx, obj, nil)
-		Expect(err).NotTo(HaveOccurred())
-	})
-
-	AfterEach(func() {
-		Expect(subject.Close()).To(Succeed())
-	})
-
-	It("reads", func() {
-		data, err := ioutil.ReadAll(subject)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(len(data)).To(BeNumerically("~", 110, 20))
-		Expect(subject.NumRead()).To(Equal(0))
-	})
-
-	It("decodes", func() {
-		var msgs []*testdata.MockMessage
-		for {
-			var msg testdata.MockMessage
-			err := subject.Decode(&msg)
-			if err == io.EOF {
-				break
-			}
+			var err error
+			subject, err = feedx.NewReader(ctx, obj, nil)
 			Expect(err).NotTo(HaveOccurred())
-			msgs = append(msgs, &msg)
-		}
+		})
 
-		Expect(msgs).To(ConsistOf(seed(), seed(), seed()))
-		Expect(subject.NumRead()).To(Equal(3))
+		AfterEach(func() {
+			Expect(subject.Close()).To(Succeed())
+		})
+
+		It("reads", func() {
+			data, err := io.ReadAll(subject)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(data)).To(BeNumerically("~", 110, 5))
+			Expect(subject.NumRead()).To(Equal(0))
+		})
+
+		It("decodes", func() {
+			var msgs []*testdata.MockMessage
+			for {
+				var msg testdata.MockMessage
+				err := subject.Decode(&msg)
+				if err == io.EOF {
+					break
+				}
+				Expect(err).NotTo(HaveOccurred())
+				msgs = append(msgs, &msg)
+			}
+
+			Expect(msgs).To(ConsistOf(seed(), seed(), seed()))
+			Expect(subject.NumRead()).To(Equal(3))
+		})
 	})
+
+	Describe("MultiReader", func() {
+		BeforeEach(func() {
+			obj = bfs.NewInMemObject("path/to/file.json")
+			Expect(writeMulti(obj, 3, time.Time{})).To(Succeed())
+
+			var err error
+			subject, err = feedx.MultiReader(ctx, []*bfs.Object{obj, obj}, nil)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			Expect(subject.Close()).To(Succeed())
+		})
+
+		It("reads", func() {
+			data, err := io.ReadAll(subject)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(data)).To(BeNumerically("~", 220, 5))
+			Expect(subject.NumRead()).To(Equal(0))
+		})
+
+		It("decodes", func() {
+			var msgs []*testdata.MockMessage
+			for {
+				var msg testdata.MockMessage
+				err := subject.Decode(&msg)
+				if err == io.EOF {
+					break
+				}
+				Expect(err).NotTo(HaveOccurred())
+				msgs = append(msgs, &msg)
+			}
+
+			Expect(msgs).To(Equal([]*testdata.MockMessage{seed(), seed(), seed(), seed(), seed(), seed()}))
+			Expect(subject.NumRead()).To(Equal(6))
+		})
+	})
+
 })
